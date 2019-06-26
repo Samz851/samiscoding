b14cdbb3057382e54b35b6e0e78b453eb6155be4
Samer Alotaibi <sam.otb@hotmail.ca>
2019-06-26 14:35:36 -0400

    form

--- packages/pagekit/blog/views/posts.php
+++ packages/pagekit/blog/views/posts.php
@@ -107,3 +107,112 @@
 
 </ul>
 <?php endif ?>
+<!-- MODAL -->
+<div class="" tabindex="-1" role="dialog" id="">
+  <div class="modal-dialog" role="document">
+<!-- contact
+    ================================================== -->
+    <section id="contact" class="s-contact">
+
+        <div class="overlay"></div>
+
+        <div class="row section-header" data-aos="fade-up">
+        <div class="col-full">
+                <h3 class="subhead">Contact SAM</h3>
+                <h1 class="display-2 display-2--light">Have a new project or an Idea? Reach Out</h1>
+            </div>
+        </div>
+
+        <div class="row contact-content" data-aos="fade-up">
+            
+            <div class="contact-primary">
+
+                <h3 class="h6">Send A Message</h3>
+
+                <form name="contactForm" id="contactForm" method="post" action="" novalidate="novalidate">
+                    <fieldset>
+    
+                    <div class="form-field">
+                        <input name="contactName" type="text" id="contactName" placeholder="Your Name" value="" minlength="2" required="" aria-required="true" class="full-width">
+                    </div>
+                    <div class="form-field">
+                        <input name="contactEmail" type="email" id="contactEmail" placeholder="Your Email" value="" required="" aria-required="true" class="full-width">
+                    </div>
+                    <div class="form-field">
+                        <input name="contactSubject" type="text" id="contactSubject" placeholder="Subject" value="" class="full-width">
+                    </div>
+                    <div class="form-field">
+                        <textarea name="contactMessage" id="contactMessage" placeholder="Your Message" rows="10" cols="50" required="" aria-required="true" class="full-width"></textarea>
+                    </div>
+                    <div class="form-field">
+                        <button class="full-width btn--primary">Submit</button>
+                        <div class="submit-loader">
+                            <div class="text-loader">Sending...</div>
+                            <div class="s-loader">
+                                <div class="bounce1"></div>
+                                <div class="bounce2"></div>
+                                <div class="bounce3"></div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    </fieldset>
+                </form>
+
+                <!-- contact-warning -->
+                <div class="message-warning">
+                    Something went wrong. Please try again.
+                </div> 
+            
+                <!-- contact-success -->
+                <div class="message-success">
+                    Your message was sent, thank you!<br>
+                </div>
+
+            </div> <!-- end contact-primary -->
+
+            <div class="contact-secondary">
+                <div class="contact-info">
+
+                    <h3 class="h6 hide-on-fullwidth">Contact Info</h3>
+
+                    <div class="cinfo">
+                        <h5>Where to Find SAM</h5>
+                        <p>
+                            1090 Kristin Way<br>
+                            Ottawa, ON<br>
+                            K1K4B6 Canada
+                        </p>
+                    </div>
+
+                    <div class="cinfo">
+                        <h5>Email SAM At</h5>
+                        <p>
+                            SAM@samiscoding.com<br>
+                            sam.otb@hotmail.ca
+                        </p>
+                    </div>
+                    <ul class="contact-social">
+                        <h5>Follow SAM On</h5>
+                        <li>
+                            <a href="https://medium.com/@samzota"><i class="fa fa-medium" aria-hidden="true"></i> <span class="contact-social">Medium</span></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/samzota/"><i class="fa fa-linkedin" aria-hidden="true"></i> <span class="contact-social">LinkedIn</span></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Samz851"><i class="fa fa-github" aria-hidden="true"></i> <span class="contact-social">Github</span></a>
+                        </li>
+                    </ul> <!-- end contact-social -->
+
+                </div> <!-- end contact-info -->
+            </div> <!-- end contact-secondary -->
+
+        </div> <!-- end contact-content -->
+
+    </section> <!-- end s-contact -->
+
+  </div>
+</div>
+
+<!-- END OF MODAL -->
